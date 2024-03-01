8a207725f59bcd63ec607a5010a92f27de83d3ac
Grayson Mckenzie <graysonmckenzie01@gmail.com>
2024-02-29 17:20:10 -0700

    updated method feedback, fixed node_message

--- Modules/Node.py
+++ Modules/Node.py
@@ -1,32 +1,48 @@
 from p2pnetwork.node import Node
 class MyNode (Node):
+    
+
     def __init__(self, host, port, id=None, callback=None, max_connections=0, debug=False):
         super(MyNode, self).__init__(host, port, id, callback, max_connections)
         print("MyNode: Start")
 
+    @property
+    def Connections(self):
+        return self.connections
+
     def outbound_node_connected(self, node):
         if (self.debug):
             print("outbound_node_connected (" + self.id + "): " + node.id)
-        
+        else:
+            print("\nConnection Sucessfull:\n! ")
+            
     def inbound_node_connected(self, node):
         if (self.debug):
             print("inbound_node_connected: (" + self.id + "): " + node.id)
+        else:
+            print("\nRemote Node Connected:\n! ")
 
     def inbound_node_disconnected(self, node):
         if (self.debug):
             print("inbound_node_disconnected: (" + self.id + "): " + node.id)
+        else:
+            print("\nRemote Node Disconnected:\n? ")
 
     def outbound_node_disconnected(self, node):
         if (self.debug):
             print("outbound_node_disconnected: (" + self.id + "): " + node.id)
+        else:
+            print("\nSucessfully Disconnected:\n? ")
 
     def node_message(self, node, data):
         if (self.debug):
             print("node_message (" + self.id + ") from " + node.id + ": " + str(data))
+        else:
+            print("Message: " + str(data))
         
     def node_disconnect_with_outbound_node(self, node):
         if (self.debug):
-            print("node wants to disconnect with oher outbound node: (" + self.id + "): " + node.id)
+            print("node wants to disconnect with other outbound node: (" + self.id + "): " + node.id)
         
     def node_request_to_stop(self):
         if (self.debug):

377a988e514372a86c3868383fc5b1525ed17098
Aubrey Morgan <amorga20@uccs.edu>
2024-03-16 16:43:58 -0600

    ECEIS update

---
+++ Modules/OurECEIS.py
@@ -0,0 +1,14 @@
+from Modules import AES,generate_points
+
+shared_key_point = generate_points.generate_points()
+key_equal_x_value = shared_key_point[0]
+
+def encrypt(message):
+    ciphertext, tag, nonce = AES.encrypt(shared_key_point, message)
+    return ciphertext, tag, nonce
+
+def decrypt(key_equal_x_value, ciphertext, tag, nonce):
+    message = AES.decrypt(key_equal_x_value, ciphertext, tag, nonce)
+    return message
+
+CT = encrypt('some message')
